cebc378bdea84c12e48cdafbb2574975e64c4ef6
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-01 01:51:33 +0330

    🎉 folders model added

--- file_storage/folders/views.py
+++ file_storage/folders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 import os
 from django.conf import settings
 from django.http import HttpResponse
+from folders.models import Folder
 
 
 def new_folder(request):
@@ -11,5 +12,9 @@ def new_folder(request):
         folder_path = os.path.join(settings.MEDIA_ROOT, folder_name)
 
         os.makedirs(folder_path)
+
+        Folder.objects.create(
+            name = folder_name.get('folder_name')
+        )
 
         return HttpResponse(f"folder was created. ")
