260eddc904a4019e4c51056805c5e5aa874b77e9
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-01 00:13:39 +0330

    🎉 folder adding view added

--- file_storage/folders/views.py
+++ file_storage/folders/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+import os
+from django.conf import settings
+from django.http import HttpResponse
 
-# Create your views here.
+
+def new_folder(request):
+    if request.method == 'POST':
+        folder_name = request.POST.get('folder_name')
+
+        folder_path = os.path.join(settings.MEDIA_ROOT, folder_name)
+
+        os.makedirs(folder_path)
+
+        return HttpResponse(f"folder was created. ")
