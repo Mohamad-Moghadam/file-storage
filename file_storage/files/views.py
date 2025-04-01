37bfaf482b5c8c3a60c96e06457eb66af20590ba
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-01 18:29:46 +0330

    🎉 view for adding new file added

--- file_storage/files/views.py
+++ file_storage/files/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
+from folders.models import Folder
+import os
+from files.models import File
+from django.http import HttpResponse
+from django.conf import settings
 
-# Create your views here.
+
+@csrf_exempt
+def new_file(request, destination_folder: str):
+    if request.method == 'POST':
+        file_name = request.POST.get('file_name')
+        folder = get_object_or_404(Folder, name= destination_folder)
+        folder_path = os.path.join(settings.MEDIA_ROOT, folder.name)
+        file_path = os.path.join(folder_path, file_name)
+        with open(file_path, 'w') as f:
+                pass
+
+        File.objects.create(
+            name = file_name,
+        )
+
+        return HttpResponse(f"file created. ")
