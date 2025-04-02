84131756284aaa4fc037b040be0ce135947a7f84
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-02 19:17:54 +0330

    🎉 showing all files url added

--- file_storage/files/urls.py
+++ file_storage/files/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from files.views import new_file
+from files.views import new_file, ls_files
 
 
 urlpatterns = [
-    path('new-file/<str:destination_folder>', new_file)
+    path('new-file/<str:destination_folder>', new_file),
+    path('ls_files/<str:folder>', ls_files),
 ]
