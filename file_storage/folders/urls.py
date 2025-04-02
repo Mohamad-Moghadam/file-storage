0716c259848f77aea31516cfd7a0038a91fd396e
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-02 19:36:59 +0330

    🐛 url fixed

--- file_storage/folders/urls.py
+++ file_storage/folders/urls.py
@@ -4,5 +4,5 @@ from folders.views import new_folder, ls_folders, rm_folder
 urlpatterns = [
     path('new-folder', new_folder),
     path('ls', ls_folders),
-    path('rm/str:folder', rm_folder),
+    path('rm/<str:folder>', rm_folder),
 ]
