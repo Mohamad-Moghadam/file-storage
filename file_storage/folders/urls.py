f9597a6ecc26303b88e6c9e670fbbe13b1c64048
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-02 19:06:17 +0330

    🎉 showing all folders url added

--- file_storage/folders/urls.py
+++ file_storage/folders/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from folders.views import new_folder
+from folders.views import new_folder, ls_folders
 
 urlpatterns = [
     path('new-folder', new_folder),
+    path('ls', ls_folders),
 ]
