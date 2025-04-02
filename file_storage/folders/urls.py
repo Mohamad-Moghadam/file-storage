999d1258e62a4a4925ff9d9622a93a2aafa7cd51
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-02 19:35:02 +0330

    🎉 removing folder url added

--- file_storage/folders/urls.py
+++ file_storage/folders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from folders.views import new_folder, ls_folders
+from folders.views import new_folder, ls_folders, rm_folder
 
 urlpatterns = [
     path('new-folder', new_folder),
     path('ls', ls_folders),
+    path('rm/str:folder', rm_folder),
 ]
