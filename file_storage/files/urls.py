9fb9935dda51e4c2302ad50cd73a6a8b29179ae0
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-02 20:25:52 +0330

    🎉 removing file url added

--- file_storage/files/urls.py
+++ file_storage/files/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from files.views import new_file, ls_files
+from files.views import new_file, ls_files, rm_file
 
 
 urlpatterns = [
     path('new-file/<str:destination_folder>', new_file),
     path('ls/<str:folder>', ls_files),
+    path('rm/<str:folder>/<str:file>', rm_file),
 ]
